be73886879f8767f362a83b93c8a51f8511132cf
Zaydee Dominguez-Chang <zaydominguez8223@gmail.com>
2024-11-20 22:42:39 -0500

    created function for routing w debugging - moved home to new func

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
+//import all funcs from model
+import * as MODEL from "./model";
+
 //import jquery
 import * as $ from 'jquery';
 
-//import all exported info from model
-import * as MODEL from "./model";
+//function to handle routing based on url hash
+function route() {
+    //get the hash from the current url
+    let hashTag = window.location.hash;
+
+    //remove the # from the hash and use "home" as default if hash is empty
+    let pageID = hashTag.replace("#", "") || "home"; 
+
+    //get content for current page id using model function
+    const pageContent = MODEL.getPageContent(pageID);
+   // console.log("Page Content:", pageContent);
+
+   //replace content of app div with new page content
+    $("#app").html(pageContent);
+
+    //re init any event listeners for new content
+    initListeners();
+}
+
 
 function initListeners() {
-    console.log("ready");
 }
- 
-$(document).ready(function () {
 
-initListeners();
+$(document).ready(() => {
+
+    //set up listener to call the route func whenever hash in the url changes
+    $(window).on("hashchange", route);
+
+    //call the route function to load appropriate page content
+    route();
 });
--- src/model.js
+++ src/model.js
@@ -16,7 +16,40 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 
-//routing content
+//function - contains all content for the app
+export function getPageContent(pageID) {
+    switch(pageID) {
+        case "home":
+            return `<!-- hero content-->
+<div class="home-hero">
+   <!-- video - constant loop -->
+   <video autoplay muted loop class="home-hero-video">
+       <source src="./images/home-hero-video.mp4" type="video/mp4">
+       <!-- in case video doesnt work -->
+       Your browser does not support the video tag.
+   </video>
+   <!-- overlay thatll go on top of video -->
+    <div class="home-hero-overlay"></div>
 
-//home 
-// KEEP GOING 
+   <!-- app title -->
+    <h1>Travel Dreamscape</h1>
+
+    <!-- app slogan -->
+     <p class="home-slogan">Your travel story begins here.</p>
+
+     <!-- app description -->
+      <p class="home-descrip">Every great adventure starts with a dream. Whether you're planning a weekend getaway or a lifelong journey, our app is designed to help you curate your personal travel bucket list. Let's turn those dreams into destinations.</p>
+     
+      <!-- btn - lead to sign up / log in -->
+       <div class="home-signlog-btn">
+           <button class="home-start"><a href="#loginSignUp" id="home-button">Start Your Adventure</a></button>
+       </div>
+   </div>`;
+
+   case "loginSignUp":
+    return `<h1>HELLO</H1>`;
+
+    default:
+        return `<h1> 404 - Page Not Found</h1>`;
+    }
+}
